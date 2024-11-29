0f272129c0740f0695668c72f0c01db52690805d
uller91 <girnikis@gmail.com>
2024-11-28 16:49:52 -0800

    added a letter count

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 def main():
-    file_contents = open_file("books/frankenstein.txt")
-    count = word_count(file_contents)
-    print(f"{count} words was found in this document")
+    file_contents = open_file("books/frankenstein.txt")     #in the solution the path is a variable
+    count_words = word_count(file_contents)
+    print(f"{count_words} words was found in this document")
+    count_characters = characters_count(file_contents)
+    print(count_characters)
 
 
+
+def characters_count(text):
+    lower_text = text.lower()   #in the solution it is done within the for loop
+    dictionary = {}
+    for letter in lower_text:
+        if letter in dictionary.keys():
+            dictionary[letter] += 1
+        else:
+            dictionary[letter] = 1
+    return dictionary
 
 
 def word_count(text):
